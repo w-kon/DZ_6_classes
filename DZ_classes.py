55fbb1f276be02a86f4ee63236a34929881a64c6
w-kon <wkon@yandex.ru>
2024-08-10 18:52:44 +0600

    2 commit

--- DZ_classes.py
+++ DZ_classes.py
@@ -7,39 +7,100 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
         
+
+    def rate_hw(self, lecturer, course, grade):
+        if (isinstance(lecturer, Lecturer) 
+            and course in lecturer.courses_attached 
+            and (course in self.courses_in_progress or course in self.finished_courses)
+            and grade >=1 and grade <=10):
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+        
+    def average_rating(self):
+        if len(self.grades) != 0:
+            resalt = []
+            for value in self.grades.values():
+                resalt.extend(value)
+                print(resalt)
+            return (sum(resalt)/len(resalt))
+        else: return "Оценок нет"
+        
+
+        
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
         self.surname = surname
         self.courses_attached = []
         
-    def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
-                student.grades[course] += [grade]
-            else:
-                student.grades[course] = [grade]
-        else:
-            return 'Ошибка'
+    
         
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.grades = {}
 
 class Reviewer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
+    def rate_hw(self, student, course, grade):
+        #print(vars(self))
+        if (isinstance(student, Student) 
+            and course in self.courses_attached 
+            and course in student.courses_in_progress):
+            if course in student.grades:
+                #print(student.name, course, grade)
+                student.grades[course] += [grade]
+            else:
+                #print('new',student.name, course, grade)
+                student.grades[course] = [grade]
+        else:
+            print(f"Студенту {student.surname} не получается поставить оценку за курс {course}")
 
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
+student_1 = Student('Ruoy', 'Eman', 'your_gender')
+student_1.courses_in_progress += ['Python', 'QA']
+student_1.finished_courses += ['Web']
+
+student_2 = Student('Mark', 'Eman', 'your_gender')
+student_2.courses_in_progress += ['Web', 'Java'] 
+student_2.finished_courses += ['Python']
+
+student_3 = Student('Mark', 'Eman', 'your_gender')
+student_3.courses_in_progress += ['Web', 'Java', 'Python'] 
+student_3.finished_courses += []
  
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+reviewer_1 = Reviewer('Some', 'Buddy')
+reviewer_1.courses_attached += ['Python']
+reviewer_1.courses_attached += ['Java', 'QA', 'Web']
+
+lecturer_1 = Lecturer('Scott', 'Buddy')
+lecturer_1.courses_attached += ['Python', 'Java', 'Web']
  
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+reviewer_1.rate_hw(student_1, 'Python', 9)
+reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'QA', 10)
+reviewer_1.rate_hw(student_2, 'Java', 8)
+reviewer_1.rate_hw(student_2, 'Web', 9)
+reviewer_1.rate_hw(student_2, 'Pyton', 8)
+
+student_1.rate_hw(lecturer_1, 'Python', 8)
+student_1.rate_hw(lecturer_1, 'Web', 1)
+student_2.rate_hw(lecturer_1, 'Web', 10)
+student_2.rate_hw(lecturer_1, 'Java', 9)
+student_2.rate_hw(lecturer_1, 'Python', 5)
+student_3.rate_hw(lecturer_1, 'Python', 8)
+student_3.rate_hw(lecturer_1, 'Java', 10)
+
+
+
+print(lecturer_1.grades)
+
+# print(student_1.grades)
+# print(student_2.grades)
+# print(Student.average_rating(student_3))
