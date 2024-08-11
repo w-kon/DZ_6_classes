2330acc93f82703c323d9f1dd9e6b22db4e67fcc
w-kon <wkon@yandex.ru>
2024-08-11 09:23:26 +0600

    5 commit

--- DZ_classes.py
+++ DZ_classes.py
@@ -41,14 +41,19 @@ class Student:
             return f"{self.name} {self.surname} {round(Student.average_rating(self), 2)} < {other.name} {other.surname} {round(Student.average_rating(other), 2)}"
         return f"{self.name} {self.surname} {round(Student.average_rating(self), 2)} > {other.name} {other.surname} {round(Student.average_rating(other), 2)}"
     
-def average_score (list_student, course):
+def average_score_student (list_student, course):
     score = []
     for student in list_student:
         if (isinstance(student, Student) and (course in student.courses_in_progress or course in student.finished_courses)):
             score.extend(student.grades[course])
-    return sum(score)/len(score)
+    return 'нет оценок' if len(score) == 0 else (sum(score)/len(score))
 
-
+def average_score_lecturer (list_lecturer, course):
+    score = []
+    for lecturer in list_lecturer:
+        if (isinstance(lecturer, Lecturer) and (course in lecturer.courses_attached)):
+            score.extend(lecturer.grades[course])
+    return 'нет оценок' if len(score) == 0 else (sum(score)/len(score))
 
 
         
@@ -108,11 +113,11 @@ class Reviewer(Mentor):
 
  
 student_1 = Student('Ruoy', 'Eman', 'your_gender')
-student_1.courses_in_progress += ['Python', 'QA']
+student_1.courses_in_progress += ['Python', 'QA', 'Java']
 student_1.finished_courses += ['Web']
 student_2 = Student('Mark', 'Eman', 'your_gender')
-student_2.courses_in_progress += ['Web', 'Java'] 
-student_2.finished_courses += ['Python']
+student_2.courses_in_progress += ['Web', 'Java', 'Python'] 
+student_2.finished_courses += ['QA']
 
  
 reviewer_1 = Reviewer('Some', 'Buddy')
@@ -120,17 +125,17 @@ reviewer_1.courses_attached += ['Python', 'QA', 'Java']
 reviewer_2 = Reviewer('Scot', 'Brin')
 reviewer_2.courses_attached += ['Java', 'QA', 'Web']
 
-lecturer_1 = Lecturer('Scott', 'Buddy')
+lecturer_1 = Lecturer('Sem', 'Braun')
 lecturer_1.courses_attached += ['Python', 'Java', 'Web'] 
 lecturer_2 = Lecturer('Simon', 'Fluc')
 lecturer_2.courses_attached += ['Java', 'Web', 'QA']
 
 reviewer_1.rate_hw(student_1, 'Python', 9)
-reviewer_1.rate_hw(student_1, 'Python', 10)
+reviewer_1.rate_hw(student_1, 'Java', 10)
 reviewer_1.rate_hw(student_1, 'QA', 10)
 reviewer_1.rate_hw(student_2, 'Java', 8)
 reviewer_1.rate_hw(student_2, 'Web', 9)
-reviewer_1.rate_hw(student_2, 'Pyton', 8)
+reviewer_1.rate_hw(student_2, 'Python', 8)
 
 student_1.rate_hw(lecturer_1, 'Python', 8)
 student_1.rate_hw(lecturer_1, 'Web', 1)
@@ -141,7 +146,11 @@ student_1.rate_hw(lecturer_2, 'Web', 1)
 student_2.rate_hw(lecturer_2, 'Web', 10)
 student_2.rate_hw(lecturer_2, 'Java', 9)
 
-list_student_test = ['Ruoy', 'Eman', 'your_gender']
+list_student_test = [student_1, student_2]
+list_lecturer_test = [lecturer_1, lecturer_2]
+course_test = 'Python'
+course_test_2 = 'Java'
+course_test_3 = 'QAA'
 
 print(student_1)
 print(student_2)
@@ -157,3 +166,8 @@ print(student_1 == student_2)
 print(student_1 < student_2)
 print(lecturer_1 == lecturer_2)
 print(lecturer_1 < lecturer_2)
+print(average_score_student(list_student_test, course_test))
+print(average_score_student(list_student_test, course_test_2))
+print(average_score_student(list_student_test, course_test_3))
+print(average_score_lecturer(list_lecturer_test, course_test))
+print(average_score_lecturer(list_lecturer_test, course_test_2))
